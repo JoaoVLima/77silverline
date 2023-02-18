fa3fe33486b7be88e7ad81a7a07445897b373d7c
JoaoVLima <celu.joaolima@gmail.com>
2023-02-18 11:26:50 -0300

    feat: configuring file structure to the project

---
+++ src/core/index.ts
@@ -0,0 +1,119 @@
+import * as THREE from 'three';
+
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls.js';
+
+import { RectAreaLightHelper } from 'three/examples/jsm/helpers/RectAreaLightHelper.js';
+import { RectAreaLightUniformsLib } from 'three/examples/jsm/lights/RectAreaLightUniformsLib.js';
+
+export default class Core {
+    public scene: THREE.Scene;
+    public camera: THREE.PerspectiveCamera;
+    public lights: THREE.Light[];
+    public renderer: THREE.WebGLRenderer;
+    public controls: OrbitControls;
+
+    constructor() {
+        this.scene = this.initScene();
+        this.camera = this.initCamera();
+        this.lights = this.initLights();
+        this.renderer = this.initRenderer();
+        this.controls = this.initControls();
+    }
+
+    /* 📦 Scene */
+    private initScene(): THREE.Scene {
+        return new THREE.Scene();
+    }
+
+    /* 🎥 Camera */
+    private initCamera(): THREE.PerspectiveCamera {
+        const camera = new THREE.PerspectiveCamera( 40, window.innerWidth / window.innerHeight, 1, 1000 );
+        camera.position.set( 0, 5, - 15 );
+        return camera;
+    }
+
+    /* 💡 Lights */
+    private initLights(): THREE.Light[] {
+        let lights: THREE.Light[] = []
+
+        RectAreaLightUniformsLib.init();
+
+        const rectLight1 = new THREE.RectAreaLight( 0xff0000, 5, 4, 10 );
+        rectLight1.position.set( - 5, 5, 5 );
+        lights.push(rectLight1)
+
+        const rectLight2 = new THREE.RectAreaLight( 0x00ff00, 5, 4, 10 );
+        rectLight2.position.set( 0, 5, 5 );
+        lights.push(rectLight2)
+
+        const rectLight3 = new THREE.RectAreaLight( 0x0000ff, 5, 4, 10 );
+        rectLight3.position.set( 5, 5, 5 );
+        lights.push(rectLight3)
+
+        this.scene.add( new RectAreaLightHelper( rectLight1 ) );
+        this.scene.add( new RectAreaLightHelper( rectLight2 ) );
+        this.scene.add( new RectAreaLightHelper( rectLight3 ) );
+
+        // const ambient = new THREE.HemisphereLight( 0xffffff, 0x444444, 0.05 );
+        // lights.push(ambient)
+        //
+        // const loader = new THREE.TextureLoader().setPath( './' );
+        // const texture = loader.load('disturb.jpg');
+        // texture.minFilter = THREE.LinearFilter;
+        // texture.magFilter = THREE.LinearFilter;
+        // texture.encoding = THREE.sRGBEncoding;
+        //
+        //
+        // const spotLight = new THREE.SpotLight( 0xffffff, 5 );
+        // spotLight.position.set( 25, 50, 25 );
+        // spotLight.angle = Math.PI / 6;
+        // spotLight.penumbra = 1;
+        // spotLight.decay = 2;
+        // spotLight.distance = 100;
+        // spotLight.map = texture;
+        //
+        // spotLight.castShadow = true;
+        // spotLight.shadow.mapSize.width = 1024;
+        // spotLight.shadow.mapSize.height = 1024;
+        // spotLight.shadow.camera.near = 10;
+        // spotLight.shadow.camera.far = 200;
+        // spotLight.shadow.focus = 1;
+        // lights.push(spotLight)
+
+        // const spotLightHelper = new THREE.SpotLightHelper(spotLight);
+        // this.scene.add(spotLightHelper)
+
+        lights.forEach((light: THREE.Light) => {
+            this.scene.add(light)
+        });
+
+        return lights;
+    }
+
+    /* 🖨️ Renderer */
+    private initRenderer(): THREE.WebGLRenderer {
+        const renderer = new THREE.WebGLRenderer();
+        renderer.setPixelRatio( window.devicePixelRatio );
+        renderer.setSize( window.innerWidth, window.innerHeight );
+        renderer.outputEncoding = THREE.sRGBEncoding;
+        renderer.shadowMap.enabled = true;
+        renderer.shadowMap.type = THREE.PCFSoftShadowMap;
+        renderer.outputEncoding = THREE.sRGBEncoding;
+        renderer.toneMapping = THREE.ACESFilmicToneMapping;
+        renderer.toneMappingExposure = 1;
+
+        document.body.appendChild( renderer.domElement );
+        return renderer;
+    }
+
+    /* 🎮 Controls */
+    private initControls(): OrbitControls {
+        const controls = new OrbitControls(this.camera, this.renderer.domElement );
+        controls.minDistance = 1;
+        controls.maxDistance = 100;
+        controls.maxPolarAngle = Math.PI / 2;
+        controls.target.set(0, 5, 0);
+        controls.update();
+        return controls;
+    }
+}
---
+++ src/index.ts
@@ -0,0 +1,103 @@
+import Core from './core'
+import * as THREE from "three";
+import {PLYLoader} from "three/examples/jsm/loaders/PLYLoader";
+import { GLTFLoader } from "three/examples/jsm/loaders/GLTFLoader.js"
+
+function init(){
+    const core = new Core();
+    const camera = core.camera
+    const scene = core.scene
+    const renderer = core.renderer
+
+    // const geometry = new THREE.PlaneGeometry( 1000, 1000 );
+    // const material = new THREE.MeshLambertMaterial( { color: 0x808080 } );
+    // const mesh = new THREE.Mesh( geometry, material );
+    // mesh.position.set( 0, - 1, 0 );
+    // mesh.rotation.x = - Math.PI / 2;
+    // mesh.receiveShadow = true;
+    // scene.add( mesh );
+
+    const geoFloor = new THREE.BoxGeometry( 2000, 0.1, 2000 );
+    const matStdFloor = new THREE.MeshStandardMaterial( { color: 0x808080, roughness: 0.1, metalness: 0 } );
+    const mshStdFloor = new THREE.Mesh( geoFloor, matStdFloor );
+    scene.add( mshStdFloor );
+
+    const geoKnot = new THREE.TorusKnotGeometry( 1.5, 0.5, 200, 16 );
+    const matKnot = new THREE.MeshStandardMaterial( { color: 0xffffff, roughness: 0, metalness: 0 } );
+    const meshKnot = new THREE.Mesh( geoKnot, matKnot );
+    meshKnot.name = 'Knot';
+    meshKnot.scale.set(0.5,0.5,0.5)
+    meshKnot.position.set( 0, 5, 0 );
+    scene.add( meshKnot );
+
+    // const objLoader = new OBJLoader()
+    // objLoader.load(
+    //     './obj/cerberus/Cerberus.obj',
+    //     (object) => {
+    //         (object.children[0] as THREE.Mesh).material = matKnot
+    //         object.traverse(function (child) {
+    //             if ((child as THREE.Mesh).isMesh) {
+    //                 (child as THREE.Mesh).material = matKnot
+    //             }
+    //         })
+    //         // object.scale.set(50,50,50)
+    //         scene.add(object)
+    //     },
+    //     (xhr) => {
+    //         console.log((xhr.loaded / xhr.total) * 100 + '% loaded')
+    //     },
+    //     (error) => {
+    //         console.log(error)
+    //     }
+    // )
+
+    new PLYLoader().load( './Lucy100k.ply', function ( geometry ) {
+
+        geometry.scale( 0.006, 0.006, 0.006 );
+        geometry.computeVertexNormals();
+
+        const material = new THREE.MeshLambertMaterial();
+
+        const mesh = new THREE.Mesh( geometry, matKnot );
+        mesh.position.y = 4.8;
+        mesh.castShadow = true;
+        mesh.receiveShadow = true;
+        mesh.name = 'Lucy';
+
+        scene.add( mesh );
+
+    } );
+
+
+
+    let onWindowResize = function () {
+
+        camera.aspect = window.innerWidth / window.innerHeight;
+        camera.updateProjectionMatrix();
+
+        renderer.setSize( window.innerWidth, window.innerHeight );
+
+    }
+
+    window.addEventListener( 'resize', onWindowResize );
+
+    let render = function () {
+        const time = performance.now() / 300;
+        // spotLight.position.x = Math.cos( time ) * 25;
+        // spotLight.position.z = Math.sin( time ) * 25;
+        // spotLight.angle = Math.sin( time );
+
+        // lightHelper.update();
+
+        const knot = scene.getObjectByName( 'Knot' ) as THREE.Mesh;
+        knot.rotation.y = time/4;
+        knot.position.x = Math.cos( time/8 ) * 4;
+        knot.position.z = Math.sin( time/8 ) * 4;
+
+        renderer.render(scene, camera);
+
+    };
+    renderer.setAnimationLoop( render );
+}
+
+init();
