7a082e4d5a1fe083063bad16dfbea410796a5bee
JoaoVLima <celu.joaolima@gmail.com>
2023-02-25 16:50:06 -0300

    changing sections offset

--- src/index.ts
+++ src/index.ts
@@ -95,12 +95,12 @@ async function init() {
 
     const detectCurrent = function (){
         const scrollY = container.scrollTop // Container scroll position
-        const goal = container.clientHeight / 2 // Where we want the current item to be, 0 = top of the container
-        console.log(goal)
+        const goal = 0 // Where we want the current item to be, 0 = top of the container
         // Find item closest to the goal
         currentItem = items.reduce((prev, curr) => {
             return (Math.abs(curr.offsetY - scrollY - goal) < Math.abs(prev.offsetY - scrollY - goal) ? curr : prev); // return the closest to the goal
         });
+
 
         let newindex = Number(currentItem.el.id)-1;
         if(index != newindex){
